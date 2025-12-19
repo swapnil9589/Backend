16d4ac7161efc76d2c0c63e4d88794354d78e8ae
swapnil95 <swapnilkarande955@gmail.com>
2025-12-19 17:38:38 +0530

    changes

--- src/middleware/log.js
+++ src/middleware/log.js
@@ -1,7 +1,15 @@
+import { error } from "console";
 import fs from "fs"
+
 export const logfiles = (req, res, next) => {
-    console.log(req.headers["user-agent"]);
-    const dateandtime = (req.headers["user-agent"] + " request time==> " + new Date().toLocaleDateString() + " " + new Date().toTimeString()).toString()
-    fs.appendFileSync("./src/text.txt", dateandtime + "\n")
+    let id =0;
+    const dateandtime = (req.headers["user-agent"] +
+        " request time==> " + new Date().toLocaleDateString() +
+        " " + new Date().toTimeString()).toString() + "\n"
+    fs.appendFile("./src/text.txt", (id) + " " + dateandtime, "utf-8", () => {
+        console.log("done");
+        console.log(id);
+    })
+    id++
     next()
 }
